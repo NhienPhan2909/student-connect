ba6594c143129b9dbc2c5e374bcac9b0b8c99f25
u7693423 <u7693423@anu.edu.au>
2024-08-22 17:51:43 +1000

    Add MongoDB database connection

--- server/routes/testDB.js
+++ server/routes/testDB.js
@@ -2,14 +2,18 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
+// MongoDB URL
+let mongoURL = "mongodb+srv://studentconnectdatabase:studentconnect@cluster0.ct0mq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 // Variable to be sent to Frontend with Database status
 let databaseConnection = "Waiting for Database response...";
+
 router.get("/", function(req, res, next) {
     res.send(databaseConnection);
 });
 
 // Connecting to MongoDB
-mongoose.connect("mongodb://mongodb:27017/test");
+mongoose.connect(mongoURL);
 
 // If there is a connection error send an error message
 mongoose.connection.on("error", error => {
